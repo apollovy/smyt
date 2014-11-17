73727ce42ccc2e34b832eb364071a34596cc7270
Yuriy A. Apollov <apollovy@gmail.com>
2014-11-17 22:14:55 +0300

    Refactor client-side into service and 2 controllers.

--- app/static/app/js/controllers.js
+++ app/static/app/js/controllers.js
@@ -1,27 +1,83 @@
-smytApp.controller(
+smytApp.factory(
+  'ModelsService',
+  [
+    '$http',
+    '$rootScope',
+    function ($http, $rootScope) {
+      var ModelsService = {
+        models: null,
+        current_model: null,
+        set_current_model: function (model) {
+          ModelsService.current_model = model;
+          $rootScope.$broadcast('current_model.update')
+        },
+        objects: null,
+        get_objects_for: function (model) {
+          $http.get(model.url).success(
+            function (data) {
+              ModelsService.objects = data;
+              $rootScope.$broadcast('objects.update');
+            }
+          )
+        }
+      };
+      $http.get(smytApp.models_path).success(
+        function(data) {
+          ModelsService.models = data;
+          $rootScope.$broadcast('models.update');
+        }
+      )
+      return ModelsService
+    }
+  ]
+).controller(
   'ModelListController',
   [
     '$scope',
     '$http',
-    function ($scope, $http) {
-      $http.get(smyt.models_path).success(
-        function(data) {
-          $scope.models = data;
-          for (model in data) {
-            var f = function() {
-              var _model = model;
-              $http.get(data[_model].url).success(
-                function(_data) {
-                  $scope.models[_model].objects = _data;
-                }
-              );
-            }();
-          }
+    'ModelsService',
+    function ($scope, $http, ModelsService) {
+
+      $scope.$on(
+        'models.update',
+        function (event) {
+          $scope.models = ModelsService.models;
         }
-      );
-      $scope.fields_for = function (model) {
-       return $scope.models[model.name].fields
+      )
+
+      $scope.show_objects_of = function (model) {
+        ModelsService.get_objects_for(model);
+        ModelsService.set_current_model(model);
       }
     }
   ]
-);
+).controller(
+  'ObjectsController',
+  [
+    '$scope',
+    '$http',
+    'ModelsService',
+    function ($scope, $http, ModelsService) {
+
+      $scope.objects = ModelsService.objects;
+      $scope.$on(
+        'objects.update',
+        function (event) {
+          $scope.objects = ModelsService.objects;
+        }
+      );
+
+      $scope.current_model = ModelsService.current_model;
+      $scope.$on(
+        'current_model.update',
+        function (event) {
+          $scope.current_model = ModelsService.current_model;
+        }
+      );
+
+      $scope.fields_for = function (model) {
+        return model ? ModelsService.models[model.name].fields : null
+      }
+    }
+  ]
+)
